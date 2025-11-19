cf8fb19ab207da72352cc954f515ce1bff6f8106
Mohan <bisunkemohan@gmail.com>
2025-11-19 12:48:17 +0545

    Data validation completed

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -16,10 +16,19 @@ TEST_FILE_NAME : str = "test.csv"
 PIPELINE_NAME : str =""
 ARTIFACT_DIR :str = "artifact"
 
+
+#schema file path
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
 ##data ingestion
 DATA_INGESTION_COLLECTION_NAME : str ="Dataset1"
 DATA_INGESTION_DIR_NAME : str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR :str = "feature_store"
 DATA_INGESTION_INGESTED_DIR : str= "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO :float = 0.25
+
+
+##data validation
+DATA_VALIDATION_DIR_NAME : str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME :str = "reports.yaml"
 
