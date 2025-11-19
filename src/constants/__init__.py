7eb0b39e2e20445d14111fea55817abcc8a2034f
Mohan <bisunkemohan@gmail.com>
2025-11-19 14:21:42 +0545

    Data Transformation completed

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,5 +1,5 @@
 import os 
-from datetime import time
+from datetime import date
 
 
 #MONGODB BASED CONSTANT
@@ -17,6 +17,10 @@ PIPELINE_NAME : str =""
 ARTIFACT_DIR :str = "artifact"
 
 
+TARGET_COLUMN = "Response"
+CURRENT_YEAR = date.today().year
+
+
 #schema file path
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
@@ -31,4 +35,11 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO :float = 0.25
 ##data validation
 DATA_VALIDATION_DIR_NAME : str = "data_validation"
 DATA_VALIDATION_REPORT_FILE_NAME :str = "reports.yaml"
+
+
+## data_transformation
+DATA_TRANSFORMATION_DIR_NAME : str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR : str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR :str ="transformed_object"
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
 
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -7,7 +7,7 @@ import yaml
 from pandas import DataFrame
 
 from src.exception import MyException
-from src.logger import logging
+from src.logger import logger
 
 
 def read_yaml_file(file_path: str) -> dict:
@@ -15,5 +15,32 @@ def read_yaml_file(file_path: str) -> dict:
         with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
 
+    except Exception as e:
+        raise MyException(e, sys) from e
+    
+def save_object(file_path: str,obj :object)->None:
+    logger.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logger.info("Exited the save_object method of utils")
+
+    except Exception as e:
+        raise MyException(e, sys) from e
+    
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
     except Exception as e:
         raise MyException(e, sys) from e
